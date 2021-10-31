a580d48b2c985f51f936662885161c4d8fd2dd27
dlchamp <36091350+dlchamp@users.noreply.github.com>
2021-10-31 12:05:10 -0500

    Initial Version of Rebuild (0.1)
    Complete rebuild.  Removed a ton of extra features I didn't need, or plan to use.  Made it simpler to just monitor select servers since I don't feel many people are wanting to monitor entire regions

--- functions.py
+++ functions.py
@@ -1,112 +1,68 @@
+import json
+import requests
+from bs4 import BeautifulSoup
 from discord_webhook import DiscordWebhook, DiscordEmbed
-from copy import deepcopy
 
 
-def discord_webhook(webhook_url, region, server, new_status, status_url, message):
-    if new_status == "✅":
-        status_color = "00cf00"
-    elif new_status == "❌":
-        status_color = "ff0000"
-    else:
-        status_color = "ffaa00"
-
-    webhook = DiscordWebhook(url=webhook_url, rate_limit_retry=True)
-    embed = DiscordEmbed(title="New World Server Status",
-                         description=message, color=status_color, url=status_url)
-    embed.add_embed_field(name="Region", value=region)
-    embed.add_embed_field(name="Server", value=server)
-    embed.add_embed_field(name="Status", value=new_status)
-    webhook.add_embed(embed)
-    response = webhook.execute()
-
-    return response
+class ServerStatus:
+    def __init__(self):
+        super().__init__()
 
 
-def switch(old_status, new_status, webhook_url, region, server, url):
-    if new_status == "✅":
-        if old_status == "❌":
-            discord_webhook(webhook_url, region, server, new_status,
-                            url, "The following server is now online.")
-        elif old_status == "null":
-            discord_webhook(webhook_url, region, server, new_status, url,
-                            "The following server has appeared on the status page.")
-        elif old_status == "⚠️":
-            discord_webhook(webhook_url, region, server, new_status, url,
-                            "The following server is no longer full.")
-    elif new_status == "❌":
-        if old_status != "null":
-            discord_webhook(webhook_url, region, server, new_status,
-                            url, "The following server is now offline. Alerts will be sent when it has come back online.")
-        elif old_status == "null":
-            discord_webhook(webhook_url, region, server, new_status, url,
-                            "The following server has appeared on the status page.")
-    elif new_status == "null":
-        discord_webhook(webhook_url, region, server, "⛔", url,
-                        "The following server has disappeared from status page.")
-    elif new_status == "⚠️":
-        discord_webhook(webhook_url, region, server, "⚠️", url,
-                        "The following server is full and could be experiencing a login queue.")
-    elif new_status == "🔧":
-        discord_webhook(webhook_url, region, server, "🔧", url,
-                        "The following server is currently down for maintenance. Alerts will be sent when it has come back online.")
+    def webhook_embed(webhook_url,server,status,message):
+        if status == "✅":
+            status_color = "00cf00"
+        elif status == "❌":
+            status_color = "ff0000"
+        else:
+            status = "ffaa00"
+
+        webhook = DiscordWebhook(url=webhook_url, rate_limit_retry=True)
+        embed = DiscordEmbed(title='Server Status', description=f'**{message}**', color=status_color)
+        embed.add_embed_field(name='Server', value=server)
+        embed.add_embed_field(name='Status', value = status)
+        embed.set_thumbnail(url='https://www.d3hell.com/wp-content/uploads/2020/08/unnamed-300x300.jpg')
+        webhook.add_embed(embed)
+        response = webhook.execute()
 
 
-def deep_diff(x, y, parent_key=None, exclude_keys=[], epsilon_keys=[]):
-    EPSILON = 0.5
-    rho = 1 - EPSILON
-
-    if x == y:
-        return None
-
-    if parent_key in epsilon_keys:
-        xfl, yfl = float_or_None(x), float_or_None(y)
-        if xfl and yfl and xfl * yfl >= 0 and rho * xfl <= yfl and rho * yfl <= xfl:
-            return None
-
-    if type(x) != type(y) or type(x) not in [list, dict]:
-        return x, y
-
-    if type(x) == dict:
-        d = {}
-        for k in x.keys() ^ y.keys():
-            if k in exclude_keys:
-                continue
-            if k in x:
-                d[k] = (deepcopy(x[k]), None)
-            else:
-                d[k] = (None, deepcopy(y[k]))
-
-        for k in x.keys() & y.keys():
-            if k in exclude_keys:
-                continue
-
-            next_d = deep_diff(
-                x[k], y[k], parent_key=k, exclude_keys=exclude_keys, epsilon_keys=epsilon_keys)
-            if next_d is None:
-                continue
-
-            d[k] = next_d
-
-        return d if d else None
-
-    d = [None] * max(len(x), len(y))
-    flipped = False
-    if len(x) > len(y):
-        flipped = True
-        x, y = y, x
-
-    for i, x_val in enumerate(x):
-        d[i] = deep_diff(y[i], x_val, parent_key=i, exclude_keys=exclude_keys, epsilon_keys=epsilon_keys) if flipped else deep_diff(
-            x_val, y[i], parent_key=i, exclude_keys=exclude_keys, epsilon_keys=epsilon_keys)
-
-    for i in range(len(x), len(y)):
-        d[i] = (y[i], None) if flipped else (None, y[i])
-
-    return None if all(map(lambda x: x is None, d)) else d
+    def compare_status(old_status,new_status):
+        diff_dict = {}
+        for key in new_status:
+            if old_status[key] != new_status[key]:
+                diff_dict[key] = new_status[key]
+        return diff_dict
 
 
-def float_or_None(x):
-    try:
-        return float(x)
-    except ValueError:
-        return None
+    def get_old_status():
+        with open('./status.json') as x:
+            old_status = json.load(x)
+        return old_status
+
+    def update_json_status(new_status):
+        with open('./status.json','w+') as f:
+            json.dump(new_status, f, indent=2)
+
+
+    def scrape_status_page(url,monitored_servers):
+        r = requests.get(url)
+        page = BeautifulSoup(r.content, 'html.parser')
+        status_section = page.find('div', class_='ags-ServerStatus-content-responses')
+        server_sections = status_section.find_all('div', class_='ags-ServerStatus-content-responses-response-server')
+
+        new_status = {}
+
+        for server in server_sections:
+            server_name = server.find('div', class_='ags-ServerStatus-content-responses-response-server-name').text.strip()
+            if server_name in monitored_servers:
+                if server.find('div',class_='ags-ServerStatus-content-responses-response-server-status ags-ServerStatus-content-responses-response-server-status--up'):
+                    new_status[server_name] = '✅'
+                if server.find('div',class_='ags-ServerStatus-content-responses-response-server-status ags-ServerStatus-content-responses-response-server-status--down'):
+                    new_status[server_name] = '❌'
+                if server.find('div',class_='ags-ServerStatus-content-responses-response-server-status ags-ServerStatus-content-responses-response-server-status--maintenance'):
+                    new_status[server_name] = '🛠️'
+                if server.find('div',class_='ags-ServerStatus-content-responses-response-server-status ags-ServerStatus-content-responses-response-server-status--full'):
+                    new_status[server_name] = '⚠️'
+        return new_status
+
+
---
+++ main.py
@@ -0,0 +1,73 @@
+from functions import ServerStatus as func
+
+
+url = 'https://www.newworld.com/en-us/support/server-status' # Should never need to change
+
+'''
+###CONFIG###
+Get your webhook link by editing the channel you wish to send webhooks, click on "Integrations" > "Webhooks"
+Click "New Webhook" > Give the webhook a name, and icon, then save.  Copy URL
+'''
+webhook_url = 'https://discord.com/api/webhooks/904276683815792680/AQpkm1j2MsabpttoY_xCTih8zCFYY4kGSL9761Ej3DwIvgu6jnFc4cTKvu1dujmknF6U' # Paste your discord channel webhook URL here
+monitored_servers = ['Valgrind','Emain Albach','Savoya'] # Add or remove which ever servers you wish to monitor
+
+
+'''
+Start server status checks and webhook sending
+old_status = gets exisitng json file, returns empty dict if none exists
+new_status = run web scrape function to get status for servers in monitored_servers
+
+'''
+old_status_dict = func.get_old_status()
+new_status_dict = func.scrape_status_page(url,monitored_servers)
+
+'''
+Check if existing JSON is empty.  If not empty, check differece in old vs new status
+diff_dict = looks for difference between old and new status, stores changed servers and new status
+Return message that correlates the status change, then sends embedded webhook, and updates
+json file for next run
+'''
+if bool(old_status_dict):
+    diff_status_dict = func.compare_status(old_status_dict,new_status_dict)
+    for server,diff_status in diff_status_dict.items():
+        if diff_statuts == '✅':
+            if old_status_dict[server] == '❌':
+                message = 'The following server is now online!'
+                func.webhook_embed(webhook_url,server,diff_status, message)
+            elif old_status_dict[server] == '🛠️':
+                message = 'The following server has completed maintenance and has come back online!'
+                func.webhook_embed(webhook_url,server,diff_status, message)
+            elif old_status_dict[server] == '⚠️':
+                message = 'The following server is no longer full and should have reduce or no wait time to log in!'
+                func.webhook_embed(webhook_url,server,diff_status, message)
+
+        elif diff_statuts == '⚠️':
+            message = 'The following server is now full!  Login queues should be expected.'
+            func.webhook_embed(webhook_url,server,diff_status, message)
+        elif diff_statuts == '❌':
+            message = 'The following server is now offline!'
+            func.webhook_embed(webhook_url,server,diff_status, message)
+        elif diff_statuts == '🛠️':
+            message = 'The following server is undergoing maintenance. A new status message will be sent when it becomes available!'
+            func.webhook_embed(webhook_url,server,diff_status, message)
+        func.update_json_status(diff_dict)
+
+else:
+    func.update_json_status(new_status_dict)
+    for server,status in new_status_dict.items():
+        if status == '✅':
+            message = 'The following server is online!'
+            func.webhook_embed(webhook_url, server, status, message)
+        elif status == '❌':
+            message = 'The following server is offline!'
+            func.webhook_embed(webhook_url, server, status, message)
+        elif status == '⚠️':
+            message = 'The following server is currently full! Loging qeueus should be expected.'
+            func.webhook_embed(webhook_url, server, status, message)
+        elif status == '🛠️':
+            message = ' The following server is undergoing maintenance! A new message status will be sent once it\'s back online.'
+            func.webhook_embed(webhook_url, server, status, message)
+
+
+
+
