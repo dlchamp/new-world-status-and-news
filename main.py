5325273dc790b7bde3168d16b9f3365ecab5638b
DLCHAMP <36091350+dlchamp@users.noreply.github.com>
2021-11-10 10:30:33 -0600

    Added logging
    Added info logging for script to see status of script each time it is ran.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from functions import ServerStatus as status_func
 from functions import GameNews as news_func
 
@@ -9,7 +10,7 @@ Paste your discord webhook channel URLs into the two variables below
 If you wish for there to only be one channel for both server status and game news (patch notes),
 make leave news_webhook_url = server_status_webhook_url
 '''
-server_status_webhook_url = 'discord-webhook-url'
+server_status_webhook_url = 'discord-web-hook-url'
 # Add second webhook url if you wish to send news messages to a different channel
 news_webhook_url = server_status_webhook_url
 '''
@@ -18,9 +19,13 @@ Assign a mention role, if you wish - Get your ID by typing \@role-you-wish-to-me
 and paste between the ''.  If you don't wish to have a role, just replace 'role-mention-id' with None (ex: mention_role = None)
 '''
 monitored_servers = ['Valgrind', 'Emain Albach', 'Savoya']
-mention_role = '<@&1234567890112354'  # Mention role or use None (no quotes)
+mention_role = None  # Mention role or use None (no quotes)
 
 ###################### END OF USER CONFIGURABLE AREA   ######################
+
+logging.basicConfig(format='%(message)s', level='INFO')
+log = logging.getLogger('root)')
+
 
 # Should never need to change
 status_url = 'https://www.newworld.com/en-us/support/server-status'
@@ -30,7 +35,9 @@ news_url = 'https://www.newworld.com/en-us/news'
 '''
 Get old server status and compare with new status, update changes and send messages to discord webhook url
 '''
+log.info('Retrieving old status from JSON')
 old_status_dict = status_func.get_old_status()
+log.info('Scraping new server status from status page')
 new_status_dict = status_func.scrape_status_page(status_url, monitored_servers)
 
 if bool(old_status_dict):
@@ -38,7 +45,7 @@ if bool(old_status_dict):
         old_status_dict, new_status_dict)
     for server, diff_status in diff_status_dict.items():
         if diff_status == '✅':
-            print(f'{server} status has been updated: ✅')
+            log.info(f'{server} status has been updated: ✅ -- Sending to Discord...')
             if old_status_dict[server] == '❌':
                 message = 'The following server is now online!'
                 status_func.webhook_embed(
@@ -53,41 +60,30 @@ if bool(old_status_dict):
                     server_status_webhook_url, server, diff_status, message, mention_role)
 
         elif diff_status == '⚠️':
-            print(f'{server} status has been updated: ⚠️')
+            log.info(f'{server} status has been updated: ⚠️-- Sending to Discord...')
             message = 'The following server is now full!  Login queues should be expected.'
             status_func.webhook_embed(
                 server_status_webhook_url, server, diff_status, message, mention_role)
         elif diff_status == '❌':
-            print(f'{server} status has been updated: ❌')
+            log.info(f'{server} status has been updated: ❌-- Sending to Discord...')
             message = 'The following server is now offline!'
             status_func.webhook_embed(
                 server_status_webhook_url, server, diff_status, message, mention_role)
         elif diff_status == '🛠️':
-            print(f'{server} status has been updated: 🛠️')
+            log.info(f'{server} status has been updated: 🛠️-- Sending to Discord...')
             message = 'The following server is undergoing maintenance. A new status message will be sent when it becomes available!'
             status_func.webhook_embed(
                 server_status_webhook_url, server, diff_status, message, mention_role)
         status_func.update_json_status(diff_dict, cwd)
 
 else:
+    '''
+    If no old status if found/JSON is empty (default), just write the current
+    status to the JSON for next status change.
+    '''
     status_func.update_json_status(new_status_dict)
-    for server, status in new_status_dict.items():
-        if status == '✅':
-            message = 'The following server is online!'
-            status_func.webhook_embed(
-                server_status_webhook_url, server, status, message, mention_role)
-        elif status == '❌':
-            message = 'The following server is offline!'
-            status_func.webhook_embed(
-                server_status_webhook_url, server, status, message, mention_role)
-        elif status == '⚠️':
-            message = 'The following server is currently full! Login qeueus should be expected.'
-            status_func.webhook_embed(
-                server_status_webhook_url, server, status, message, mention_role)
-        elif status == '🛠️':
-            message = ' The following server is undergoing maintenance! A new message status will be sent once it\'s back online.'
-            status_func.webhook_embed(
-                server_status_webhook_url, server, status, message, mention_role)
+    log.info('Current Statuses stored for next scan.')
+
 
 
 '''
@@ -98,11 +94,16 @@ old_articles_dict = news_func.get_old_articles()
 
 if bool(old_articles_dict):
     for article in new_articles_dict:
-        if new_articles_dict[article]['title'] == old_articles_dict[article]['title']:
-            pass
-        else:
+        if new_articles_dict[article]['title'] != old_articles_dict[article]['title']:
             news_func.articles_webhook(
                 news_webhook_url, new_articles_dict, mention_role)
             news_func.update_articles(new_articles_dict)
+
+
 else:
+    '''
+    If article.json is empty, (no old article to compare against)
+    just store currently scraped article to JSON for next scan
+    '''
+
     news_func.update_articles(new_articles_dict)
